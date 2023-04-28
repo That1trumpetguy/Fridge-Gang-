bdc06782d3e73b9a0d0a31dde22e64e6d347c9b3
HaysBr18 <HaysBr@cwu.edu>
2023-04-28 10:06:18 -0700

    Added a screen to edit profile info.

--- lib/EditProfilePage.dart
+++ lib/EditProfilePage.dart
@@ -9,6 +9,7 @@ class EditProfilePage extends StatefulWidget {
 }
 
 class _EditProfilePageState extends State<EditProfilePage> {
+  bool showPassword = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,6 +25,93 @@ class _EditProfilePageState extends State<EditProfilePage> {
           ),
         ),
       ),
+      body: Container(
+        padding: const EdgeInsets.only(left: 16, top: 25, right: 16),
+        child: ListView(
+          children:  [
+            const Text(
+              "Edit Profile",
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            buildTextField("Name", "Bryant Hays", false),
+            buildTextField("Email", "Bryant1234@gmail.com", false),
+            buildTextField("Password", "*******************", true),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                  OutlinedButton(
+                    onPressed:  (){},
+                      child: const Text(
+                        "Cancel",
+                        style: TextStyle(
+                          fontSize: 18,
+                          letterSpacing: 2.0,
+                          color: Colors.black
+                        ),
+                      ),
+                    style: OutlinedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(horizontal: 50),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20)
+                        ),
+                    ),),
+                ElevatedButton(
+                    onPressed: (){},
+                    child: const Text(
+                      "Save",
+                      style: TextStyle(
+                          fontSize: 18,
+                          letterSpacing: 2.0,
+                          color: Colors.black
+                      ),
+                    ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xffdbdfd1),
+                    padding: EdgeInsets.symmetric(horizontal: 50),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)
+                  ),
+                ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Padding buildTextField(String labelText, String hintTxt, bool isPasswordField) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 25),
+      child: TextField(
+              obscureText: isPasswordField ? showPassword : false,
+              decoration: InputDecoration(
+                suffixIcon: isPasswordField ? IconButton(onPressed: (){
+                  setState(() {
+                    showPassword = !showPassword;
+                  });
+                },
+                    icon: const Icon(
+                      Icons.remove_red_eye,
+                      color: Colors.grey,
+                    ),
+                ) : null,
+                  contentPadding: EdgeInsets.only(bottom: 5),
+                labelText: labelText,
+                floatingLabelBehavior: FloatingLabelBehavior.always,
+                hintText: hintTxt,
+                hintStyle: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                )
+              ),
+            ),
     );
   }
 }
