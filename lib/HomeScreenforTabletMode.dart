611ca576b0af42fc9d73e96ade90f90771143b52
Alan Tran <alantran8881@gmail.com>
2023-05-08 14:31:57 -0700

    Add a page and modified main page
    Added About to Expire list, basically taking the code from Grocery List and Repeating it. Also Modified the main page slightly, adding a little padding to the top to avoid phone tools that can be triggered at the top.

--- lib/HomeScreenforTabletMode.dart
+++ lib/HomeScreenforTabletMode.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter_app/GroceryListPage.dart';
+import 'package:flutter_app/AboutToExpireList.dart';
 import 'package:flutter_app/settings.dart';
 import 'package:flutter_app/utils.dart';
 import 'dart:ui';
@@ -20,7 +21,8 @@ class _SceneState extends State<Scene> {
     double ffem = fem * 0.97;
     return Scaffold(
       body: Container(
-      width: double.infinity,
+        padding: EdgeInsets.only(top:20),
+        width: double.infinity,
       child: Container(
         // ipadpro111Eks (1:2)
         width: double.infinity,
@@ -874,18 +876,25 @@ class _SceneState extends State<Scene> {
                         // abouttoexpiredvT (6:12)
                         alignment: Alignment.centerLeft,
                         child: SizedBox(
-                          child: Container(
-                            constraints: BoxConstraints(
-                              maxWidth: 101 * fem,
-                            ),
-                            child: Text(
-                              'About to expire!',
-                              style: SafeGoogleFont(
-                                'Inter',
-                                fontSize: 24 * ffem,
-                                fontWeight: FontWeight.w600,
-                                height: 1.2125 * ffem / fem,
-                                color: const Color(0xff000000),
+                          child: GestureDetector(
+                            onTap: (){
+                              Navigator.of(context).push(
+                                  MaterialPageRoute (
+                                      builder: (BuildContext context) => const AboutToExpireList()
+                                  ));},
+                            child: Container(
+                              constraints: BoxConstraints(
+                                maxWidth: 132 * fem,
+                              ),
+                              child: Text(
+                                'About to expire!',
+                                style: SafeGoogleFont(
+                                  'Inter',
+                                  fontSize: 24 * ffem,
+                                  fontWeight: FontWeight.w600,
+                                  height: 1.2125 * ffem / fem,
+                                  color: const Color(0xff000000),
+                                ),
                               ),
                             ),
                           ),
