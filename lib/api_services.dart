29f2d7567d397737cd9e11363fdaf4a8d447dd0b
Racheljoans <94077195+Racheljoans@users.noreply.github.com>
2023-05-15 10:38:54 -0700

    Update api_services.dart
    api recipe output

--- lib/api_services.dart
+++ lib/api_services.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+
+
 Future<Map<String, dynamic>> fetchRecipeData( int recipeId) async {
   final response = await http.get(Uri.parse(
       'https://api.spoonacular.com/recipes/$recipeId/information?apiKey=d06e57ea3ff9451985a6f2856e19b2b2'));
@@ -13,12 +15,25 @@ Future<Map<String, dynamic>> fetchRecipeData( int recipeId) async {
 
 Future<Map<String, dynamic>> fetchRandomRecipe() async {
   final response = await http.get(Uri.parse(
-      'https://api.spoonacular.com/recipes/random?apiKey=<d06e57ea3ff9451985a6f2856e19b2b2>&tags=breakfast'));
+      'https://api.spoonacular.com/recipes/random?apiKey=<d06e57ea3ff9451985a6f2856e19b2b2>'));
 
   if (response.statusCode == 200) {
-    return json.decode(response.body)['recipes'][0];
+    final recipe = json.decode(response.body)['recipes'][0];
+    print(recipe);
+    return recipe;
   } else {
     throw Exception('Failed to load recipe');
   }
 }
 
+void testFetchRandomRecipe() async {
+  try {
+    final recipe = await fetchRandomRecipe();
+    print(recipe);
+  } catch (e) {
+    print('Error: $e');
+  }
+}
+void main() {
+  testFetchRandomRecipe();
+}
